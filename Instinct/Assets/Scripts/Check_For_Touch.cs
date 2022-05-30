46f07aacd5311ead2ae9622db5ec9d48a39c5f87
Hitarth Nikeshkumar Kothari <71833799+Hitarth-Kothari@users.noreply.github.com>
2022-05-30 15:20:59 -0600

    Delete Instinct directory

--- Instinct/Assets/Scripts/Check_For_Touch.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dfcc20c7c0cc8d8ecaea5d568f638427dcf24c9384393906063fe30046a4cbb3
-size 1253
--- Instinct/Assets/Scripts/Game_Master.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7be983fdfe81fcb6cc4c7d95d2343897a8baf99c7ddae4f20fd25b971a131f27
-size 251
