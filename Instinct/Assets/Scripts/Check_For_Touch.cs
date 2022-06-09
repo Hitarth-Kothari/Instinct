d5d8289f18847d5ecb923ff9d16edf71c1477838
TheLastWonderor <71833799+Hitarth-Kothari@users.noreply.github.com>
2022-06-08 21:54:32 -0600

    made timers

--- Instinct/Assets/Scripts/Check_For_Touch.cs
+++ Instinct/Assets/Scripts/Check_For_Touch.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Check_For_Touch : MonoBehaviour
 {
+    // Sprite images
+    public Sprite one;
+    public Sprite two;
+    public Sprite three;
+    public Sprite four;
+    public Sprite five;
+    // Sprite counter
+    int sprite = 5;
     // Collider
     Collider2D Col;
     // Timer
@@ -24,7 +32,39 @@ public class Check_For_Touch : MonoBehaviour
         {
             SceneManager.LoadScene("Start");
         }
-        
+        if (timer >= 1 && timer < 2)
+        {
+            if (sprite != 4)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = four;
+                sprite = 4;
+            }
+        }
+        else if (timer >= 2 && timer < 3)
+        {
+            if (sprite != 3)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = three;
+                sprite = 3;
+            }
+        }
+        else if (timer >= 3 && timer < 4)
+        {
+            if (sprite != 2)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = two;
+                sprite = 2;
+            }
+        }
+        else if (timer >= 4 && timer < 5)
+        {
+            if (sprite != 1)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = one;
+                sprite = 1;
+            }
+        }
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -37,6 +77,8 @@ public class Check_For_Touch : MonoBehaviour
                 {
                     timer = 0;
                     Score_Manager.instance.Add_Score();
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = five;
+                    sprite = 5;
                     gameObject.GetComponent<Randomizer>().New_position();
                 }
             }
